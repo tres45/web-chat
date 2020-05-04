a5b968f88f39e1ccbfadd158a00427b84a537f71
tres45 <osadchyi91@gmail.com>
2020-05-04 16:51:27 +0300

    fix heroku

--- web-chat-client/server.js
+++ web-chat-client/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/web-chat-client'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(request, response) {
-  response.sendFile(path.join(__dirname + '/dist/index.html'));
+  response.sendFile(path.join(__dirname + '/dist/web-chat-client/index.html'));
 });
